15f2c8f976426eaf4997f10c82e9edb51d7fda2b
Rex Liu <rexliu33@gmail.com>
2020-12-24 20:40:04 -0800

    background working

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import AboutMe from "../sections/AboutMe";
 import Experiences from "../sections/Experiences";
 import Projects from "../sections/Projects";
+//import { particlesJS } from particles.js;
+import Particles from 'react-particles-js';
 
 var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
@@ -63,6 +65,8 @@ window.onload = function () {
   document.body.appendChild(css);
 };
 
+//particlesJS("particles-js", {"particles":{"number":{"value":400,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":10,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":500,"color":"#ffffff","opacity":0.4,"width":2},"move":{"enable":true,"speed":6,"direction":"bottom","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":4,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+
 class MainPage extends React.Component {
   render() {
     const togglePopup = () => {
@@ -72,8 +76,13 @@ class MainPage extends React.Component {
 
     return (
       <main className="main">
+        
         <header className="main__header">
-          <h1 className="main__header__name">Rex Liu</h1>
+        {/*<div id="particles-js"></div>
+        <script src="particles.js"></script>
+        <div class="count-particles"> <span class="js-count-particles">--</span> particles </div> <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script><script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+    */}
+        <h1 className="main__header__name">Rex Liu</h1>
           <hr className="main__header__horizontal"></hr>
           <div className="main__header__aboutOuter">
             <h2 className="main__header__about">Hi, I am a&nbsp;</h2>
@@ -113,6 +122,8 @@ class MainPage extends React.Component {
             <div class="main__header__arrow bounce"></div>
           </a>
         </header>
+        <div className="animation">
+        <Particles />  </div>
 
         <div class="main__AboutMe" id="AboutMe">
           <AboutMe />
