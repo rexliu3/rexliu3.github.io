02776f17c8b0270fa4368b6003dc27b8c96923e7
Rex Liu <rexliu33@gmail.com>
2020-12-24 17:58:24 -0800

    working on homepage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Langar&display=swap" rel="stylesheet"/>
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route path="*" component={NotFoundPage} />
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -3,63 +3,148 @@ import AboutMe from "../sections/AboutMe";
 import Experiences from "../sections/Experiences";
 import Projects from "../sections/Projects";
 
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.setAttribute("className", "main__header__about");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff}";
+  document.body.appendChild(css);
+};
 
 class MainPage extends React.Component {
   render() {
     const togglePopup = () => {
       document.getElementById("popup").classList.toggle("active");
       document.getElementById("blur").classList.toggle("active");
-    }    
+    };
+
+    const mystyle = {
+      height:'100vh',
+      //height: '265px',
+      boxSizing: 'border-box',
+      display: 'flex',
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      border: '2px solid', 
+      margin: '1em 0', 
+      padding: '1em',
+      marginBottom: '-50vh',
+    };
 
     return (
       <main className="main">
+        <p
+            className="codepen"
+            data-height="265"
+            data-theme-id="dark"
+            data-default-tab="css,result"
+            data-user="arickle"
+            data-slug-hash="XKjMZY"
+            style={mystyle}
+            data-pen-title="CSS Rain Effect"
+            
+          ></p>
+          <script
+            async
+            src="https://cpwebassets.codepen.io/assets/embed/ei.js"
+          ></script>
         <header className="main__header">
-        <nav className="main__navigation" id="nav">
-          <ul className="main__navigation__nav">
-            <li className="main__navigation__item">
-              <a className="main__navigation__link" href="#AboutMe">
-                About
-              </a>
-            </li>
-            <li className="main__navigation__item">
-              <a className="main__navigation__link" href="#Experiences">
-                Experiences
-              </a>
-            </li>
-          </ul>
-          <a className="main__navigation__logo" href="#">
-            <img
-              src="assets/Profile-Picture.jpg"
-              className="main__navigation__logo__image"
-            />
-          </a>
-          <ul className="main__navigation__nav">
-            <li className="main__navigation__item">
-              <a className="main__navigation__link" href="#Projects">
-                Projects
-              </a>
-            </li>
-            <li className="main__navigation__item">
-              <a className="main__navigation__link" href="/">
-                Skills
-              </a>
-            </li>
-          </ul>
-        </nav>
           <h1 className="main__header__name">Rex Liu</h1>
-          <h2 className="main__header__about">
-            Software Developer. Data Scientist. <br></br>
-            Badminton Player. Pianist.
-          </h2>
-          <a href="mailto: rexliu3@berkeley.edu">
-            <img className="main__header__email" src="assets/email3.png"></img>
+          <hr className="main__header__horizontal"></hr>
+          <div className="main__header__aboutOuter">
+            <h2 className="main__header__about">Hi, I am a&nbsp;</h2>
+            <h2 className="main__header__about">
+              <div
+                href=""
+                class="typewrite"
+                data-period="2000"
+                data-type='[ "Leader.", "Tech Enthusiast.", "Learner." , "Web Developer"]'
+              >
+                <span class="wrap"></span>
+              </div>
+            </h2>
+          </div>
+          <div className="main__header__logos">
+            <a href="https://github.com/rexliu3">
+              <img
+                className="main__header__logos__logo"
+                src="assets/GitHub-Logo.png"
+              ></img>
+            </a>
+            <a href="https://linkedin.com/in/rexliu3">
+              <img
+                className="main__header__logos__logo"
+                src="assets/linkedin.png"
+              ></img>
+            </a>
+            <a href="mailto: rexliu3@berkeley.edu">
+              <img
+                className="main__header__logos__logo"
+                src="assets/email3.png"
+              ></img>
+            </a>
+          </div>
+
+          <a href="#">
+            <div class="main__header__arrow bounce"></div>
           </a>
         </header>
-        
+
         <div class="main__AboutMe" id="AboutMe">
           <AboutMe />
         </div>
-
 
         <div class="main__Experiences" id="Experiences">
           <Experiences />
@@ -71,35 +156,62 @@ class MainPage extends React.Component {
 
         <footer className="main__footer">
           <div id="newline">
-          {/*} className="divider" id="footerOne" viewBox="0 0 100 100" preserveAspectRatio="none">
+            {/*} className="divider" id="footerOne" viewBox="0 0 100 100" preserveAspectRatio="none">
             <path d="M0 0 L50 100 L100 0 Z"></path>
           </svg>*/}
-          <div className="divider" id="footerOne"/>
-          <ul className="main__footer__navitems">
+            <div className="divider" id="footerOne" />
+            <ul className="main__footer__navitems">
               <li className="main__footer__item">
-                <a className="main__footer__link" href="https://linkedin.com/in/rexliu3" target="_blank">
-                  <img className="main__footer__logo" src="assets/linkedin.png"></img>
+                <a
+                  className="main__footer__link"
+                  href="https://linkedin.com/in/rexliu3"
+                  target="_blank"
+                >
+                  <img
+                    className="main__footer__logo"
+                    src="assets/linkedin.png"
+                  ></img>
                 </a>
               </li>
               <li className="main__footer__item">
-                <a className="main__footer__link" href="https://github.com/rexliu3" target="_blank">
-                  <img className="main__footer__logo" src="assets/github.png"></img>
+                <a
+                  className="main__footer__link"
+                  href="https://github.com/rexliu3"
+                  target="_blank"
+                >
+                  <img
+                    className="main__footer__logo"
+                    src="assets/github.png"
+                  ></img>
                 </a>
               </li>
               <li className="main__footer__item">
-                <a className="main__footer__link" href="https://instagram.com/rexliu3" target="_blank">
-                  <img className="main__footer__logo" src="assets/instagram.png"></img>
+                <a
+                  className="main__footer__link"
+                  href="https://instagram.com/rexliu3"
+                  target="_blank"
+                >
+                  <img
+                    className="main__footer__logo"
+                    src="assets/instagram.png"
+                  ></img>
                 </a>
               </li>
               <li className="main__footer__item">
-                <a className="main__footer__link" href="https://facebook.com/rexliu333" target="_blank">
-                  <img className="main__footer__logo" src="assets/facebook.png"></img>
+                <a
+                  className="main__footer__link"
+                  href="https://facebook.com/rexliu333"
+                  target="_blank"
+                >
+                  <img
+                    className="main__footer__logo"
+                    src="assets/facebook.png"
+                  ></img>
                 </a>
               </li>
-          </ul>
-          
+            </ul>
 
-          <ul className="main__footer__nav">
+            <ul className="main__footer__nav">
               <li className="main__footer__item">
                 <a className="main__footer__link" href="#AboutMe">
                   About
@@ -121,7 +233,7 @@ class MainPage extends React.Component {
                 </a>
               </li>
             </ul>
-            </div>
+          </div>
         </footer>
       </main>
     );
