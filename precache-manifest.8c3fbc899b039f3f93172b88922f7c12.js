3f939a381861f8e94bfa365259a529e0f00d9003
Rex Liu <rexliu33@gmail.com>
2021-05-20 00:46:34 -0700

    Updates

---
+++ precache-manifest.8c3fbc899b039f3f93172b88922f7c12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d45825d48a2ff47df9350d77b15207",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f4037fc603cfa78a0ca",
+    "url": "/static/css/main.50b27478.chunk.css"
+  },
+  {
+    "revision": "a07fac9d972760e450e1",
+    "url": "/static/js/2.ef56eb00.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ef56eb00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4037fc603cfa78a0ca",
+    "url": "/static/js/main.5c713e5a.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "4bbcad3ba6c1e407a7d56f5a4ef04316",
+    "url": "/static/media/teal.4bbcad3b.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fa184b9f4a8c7b601fa761607045235.js"
+  "/precache-manifest.8c3fbc899b039f3f93172b88922f7c12.js"
 );
 
 self.addEventListener('message', (event) => {
